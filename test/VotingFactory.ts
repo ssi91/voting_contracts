14b1e2ee446a8c852ba77d799054b49b662b9794
ssi91 <sergeev.s.i1991@gmail.com>
2023-08-30 22:45:02 -0400

    add test for VotingFactory contract

---
+++ test/VotingFactory.ts
@@ -0,0 +1,35 @@
+import {ethers} from "hardhat";
+import {time} from "@nomicfoundation/hardhat-network-helpers";
+import {VotingFactory} from "../typechain-types";
+import {encodeBytes32String} from "ethers";
+import {seconds} from "@nomicfoundation/hardhat-network-helpers/dist/src/helpers/time/duration";
+import {expect} from "chai";
+import {HardhatEthersSigner} from "@nomicfoundation/hardhat-ethers/signers";
+
+describe("VotingFactory", function () {
+    let votingFactory: VotingFactory;
+    let accounts: HardhatEthersSigner[];
+    beforeEach(async () => {
+        const VotingFactory = await ethers.getContractFactory("VotingFactory");
+        votingFactory = await VotingFactory.deploy();
+        await votingFactory.waitForDeployment();
+        accounts = await ethers.getSigners();
+    });
+
+    it("Should create a voting instance", async () => {
+        const deadline = await time.latest() + 100;
+        const titles = [encodeBytes32String("option1"), encodeBytes32String("option2")];
+
+        const instanceTx = await votingFactory.createVoting(deadline, titles);
+        const tx = await instanceTx.wait();
+
+        // @ts-ignore
+        const args = tx?.logs.filter((event) => event.fragment.name === "VotingInstanceCreated")[0].args;
+
+        const votingAddress = args[0];
+        const voting = await ethers.getContractAt("Voting", votingAddress);
+
+        const chairmanAddress = await voting.chairman();
+        expect(chairmanAddress).to.be.equal(accounts[0].address)
+    });
+});
